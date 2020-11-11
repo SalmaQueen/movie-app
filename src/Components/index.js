5a916bfccd9e0fc312f95ad5315d4af4b37600b4
salmaQueen <salmawasee9@gmail.com>
2020-11-11 10:58:07 +0300

    initial commit

---
+++ src/Components/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchMovies from "./SearchMovies";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">Movie Search</h1>
+        <SearchMovies/>
+      </div>
+    );
+  }
+}
+export default Main
